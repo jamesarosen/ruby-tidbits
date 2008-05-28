141792ef9797ba56189308f353a9f2683f61e6aa
James Rosen <james@u-presence.com>
2008-05-28 15:26:15 -0400

    moved includes from init.rb to rails.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,4 @@
 # an init.rb file so the project can act like a Rails plugin
 if ENV['RAILS_ENV']
   require File.join(File.dirname(__FILE__), 'lib', 'tidbits', 'rails')
-  ActiveRecord::Base.send :include, Tidbits::ActiveRecord::ValidatesEmail
-  ActiveRecord::Base.send :include, Tidbits::ActiveRecord::ValidatesUri
 end
--- lib/tidbits/rails.rb
+++ lib/tidbits/rails.rb
@@ -1,5 +1,7 @@
 require 'tidbits/active_record/validates_email'
 require 'tidbits/active_record/validates_uri'
+ActiveRecord::Base.send :include, Tidbits::ActiveRecord::ValidatesEmail
+ActiveRecord::Base.send :include, Tidbits::ActiveRecord::ValidatesUri
 
 module Tidbits
   module Rails
